685fd5e0d36871bb238a4a5c799dc13f8ac8af14
MUH. AHSAN <muh.ahsan.student@gmail.com>
2022-09-23 03:17:42 +0700

    Uploaded assignment day fourth

---
+++ fw-fazztrack-day-four/assignment-day/biodata.js
@@ -0,0 +1,44 @@
+const biodata = {
+  name: "MUH. AHSAN",
+  age: 72,
+  hobbies: [
+    "futsal",
+    "reading"],
+  isMarriade: false,
+  schoolList: [
+    {
+      name: "SDN 089 Masamba",
+      yearIn: 2006,
+      yearOut: 2012,
+      major: null
+    },
+    {
+      name: "SMPN 01 Masamba",
+      yearIn: 2012,
+      yearOut: 2015,
+      major: null
+    },
+    {
+      name: "SMAN 02 Masamba",
+      yearIn: 2015,
+      yearOut: 2017,
+      major: "Informatics Engineering"
+    },
+
+    {
+      name: "Universitas Mercu Buana",
+      yearIn: 2018,
+      yearOut: 2022,
+      major: "Informatics Engineering"
+    },
+  ],
+  skill: [
+    {
+      skillName: "Javascript",
+      level: "beginner"
+    },
+  ],
+  interestInCoding: true,
+};
+
+// console.log(biodata)
---
+++ fw-fazztrack-day-four/assignment-day/data.js
@@ -0,0 +1,45 @@
+let data = {
+  id: 1,
+  name: "Leanne Graham",
+  username: "Bret",
+  email: "sincere@april.biz",
+  address: 
+  {
+    street: "Kulas Light",
+    suite: "Apt. 556",
+    city: "Gwenborough",
+    zipcode: "92998=3874",
+  },
+  phone: "1-770-736-8032 x56442",
+  webiste: "hildegard.org",
+}
+
+console.log(data)
+
+// a. Spread Operator
+
+/*Object.assign(data,{
+  name:"MUH. AHSAN",
+  username:"muhahsanarifin",
+  email:"muh.ahsan.student@gmail.com"
+})*/
+
+const newData = {
+  name: "MUH. AHSAN",
+  username: "muhahsanarifin",
+  email: "muh.ahsan.student@gmail.com"
+}
+
+const margedData = {...data, ...newData}
+
+console.log(margedData)
+
+
+// b. Destrucutring
+
+const {street, city} = data.address;
+
+console.log(`
+  Jalan ${street}
+  City ${city}
+`)
