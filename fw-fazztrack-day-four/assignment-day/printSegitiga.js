040a09934cd56b474a2ed30c9cca9a884af9b4de
MUH. AHSAN <muh.ahsan.student@gmail.com>
2022-09-23 06:26:20 +0700

    Did comment in line 11 and 14

--- fw-fazztrack-day-four/assignment-day/printSegitiga.js
+++ fw-fazztrack-day-four/assignment-day/printSegitiga.js
@@ -8,10 +8,10 @@ const printSegitigaProcess = (value) => {
     for (let index = value; index > 0; index--) {
       for (let join = 1; join <= index; join++) {
         number += join;
-        console.log(number)
+        // console.log(number); 
       }
-      number += `\n`;
-        console.log(number);
+        number += `\n`;
+        // console.log(number)
     }
     console.log(number);
   }
@@ -20,7 +20,6 @@ const printSegitigaProcess = (value) => {
 const notNumberalert = () => {
   console.log("Must be number type data");
 };
-
 
 const printSegitiga = 5 ;
 printSegitigaProcess(printSegitiga);
