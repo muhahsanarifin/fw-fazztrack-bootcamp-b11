5745b722e16701086016d0b094975447e628ee64
MUH. AHSAN <muh.ahsan.student@gmail.com>
2022-09-25 18:41:07 +0700

    Updated folders and files

---
+++ fw-fazztrack-week-one/fw-fazztrack-day-four/assignment-day/printTriangle.js
@@ -0,0 +1,29 @@
+const printSegitigaProcess = (value) => {
+
+    let number = "";
+    for (let index = value; index > 0; index--) {
+      for (let join = 1; join <= index; join++) {
+        number += join;
+      }
+        number += `\n`;
+    }
+    console.log(number);
+  }
+
+
+const notNumberalert = () => {
+  console.log("Must be number type data");
+};
+
+const printSegitiga = 5;
+
+ if (typeof printSegitiga == "number") {
+
+    printSegitigaProcess(printSegitiga);
+    
+
+  } else {
+
+    return notNumberalert();
+
+}
